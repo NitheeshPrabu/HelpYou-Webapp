aee5932df1bfc54d25fcca5cd1e39ad5b63da850
Nitheesh <nitheeshprabu.s@gmail.com>
2018-05-22 17:08:02 +0530

    resetting repo

--- src/common/common.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-'use strict';
-
-angular.module('common', [])
-.constant('ApiPath', 'https://mcodd-course5.herokuapp.com')
-
-})();
--- src/public/public.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-"use strict";
-/**
- * Public HelpYou application. Includes the common module and ui-router.
- */
-angular.module('public', ['ui.router', 'common']);
-
-})();
